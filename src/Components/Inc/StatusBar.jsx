f5479c0de947cea092034fc00c467d63d675328a
devArustu <devendra.arustu@gmail.com>
2025-09-26 11:48:44 +0530

    slver rr

--- src/Components/Inc/StatusBar.jsx
+++ src/Components/Inc/StatusBar.jsx
@@ -1,13 +1,27 @@
 import React, { useState, useEffect, useRef, forwardRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck, faTimes, faExclamation, faInfo, } from "@fortawesome/free-solid-svg-icons";
+import { useLocation, useNavigate } from "react-router-dom";
 
 // ✅ forwardRef use correctly
 const StatusBar = forwardRef((props, ref) => {
 
   const pathname = window.location.pathname;
+  const navigate = useNavigate();
 
   const { loading, incidentErrorStatus, offenseCount, offenseErrorStatus, NameCount, nameErrorStatus, NameRelationshipError, narrativeApprovedStatus, PropertyCount, PropErrorStatus } = props;
+
+  const useQuery = () => {
+    const params = new URLSearchParams(useLocation().search);
+    return {
+      get: (param) => params.get(param)
+    };
+  };
+
+  const query = useQuery();
+  var IncID = query?.get("IncId");
+  var IncNo = query?.get("IncNo");
+  var IncSta = query?.get("IncSta");
 
 
   const statusData = [
@@ -18,6 +32,7 @@ const StatusBar = forwardRef((props, ref) => {
       color: "text-grays",
       bg: incidentErrorStatus ? "bg-red" : "bg-green",
       icon: incidentErrorStatus ? faTimes : faCheck,
+      link: `/Inc-Home?IncId=${IncID}&IncNo=${IncNo}&IncSta=${IncSta}`,
     },
     {
       label: "Offense",
@@ -27,6 +42,7 @@ const StatusBar = forwardRef((props, ref) => {
       color: "text-grays",
       bg: offenseCount && !offenseErrorStatus ? "bg-green" : "bg-red",
       icon: offenseCount && !offenseErrorStatus ? faCheck : faTimes,
+      link: `/Off-Home?IncId=${IncID}&IncNo=${IncNo}&IncSta=${IncSta}`,
     },
     {
       label: "Name",
@@ -35,6 +51,7 @@ const StatusBar = forwardRef((props, ref) => {
       color: "text-grays",
       bg: NameCount && !nameErrorStatus ? "bg-green" : "bg-red",
       icon: NameCount && !nameErrorStatus ? faCheck : faTimes,
+      link: `/Name-Home?IncId=${IncID}&IncNo=${IncNo}&IncSta=${IncSta}`,
     },
     {
       label: "OV Links",
@@ -43,6 +60,7 @@ const StatusBar = forwardRef((props, ref) => {
       color: "text-grays",
       bg: !NameRelationshipError ? "bg-red" : "bg-green",
       icon: !NameRelationshipError ? faTimes : faCheck,
+      link: `/Name-Home?IncId=${IncID}&IncNo=${IncNo}&IncSta=${IncSta}`,
     },
     {
       label: "Report",
@@ -51,6 +69,7 @@ const StatusBar = forwardRef((props, ref) => {
       color: "text-grays",
       bg: narrativeApprovedStatus ? "bg-green" : "bg-yellow",
       icon: narrativeApprovedStatus ? faCheck : faExclamation,
+      link: `/Inc-Report?IncId=${IncID}&IncNo=${IncNo}&IncSta=${IncSta}`,
     },
     // {
     //   label: "Property",
@@ -66,6 +85,7 @@ const StatusBar = forwardRef((props, ref) => {
       border: typeof PropertyCount === "number" && !PropErrorStatus ? "left-border-green" : "left-border-red",
       bg: typeof PropertyCount === "number" && !PropErrorStatus ? "bg-green" : "bg-red",
       icon: typeof PropertyCount === "number" && !PropErrorStatus ? faCheck : faTimes,
+      link: `/Prop-Home?IncId=${IncID}&IncNo=${IncNo}&IncSta=${IncSta}`,
     },
     {
       label: "TIBRS",
@@ -101,7 +121,6 @@ const StatusBar = forwardRef((props, ref) => {
   };
 
 
-
   return (
     <div ref={ref} className="d-flex flex-column gap-3 mb-2" style={{
       rowGap: "6px",
@@ -109,7 +128,7 @@ const StatusBar = forwardRef((props, ref) => {
       // top: 50,
       // left: 0,
       // right: 41,
-      // zIndex: 1050, 
+      // zIndex: 1050,   
       backgroundColor: "#fff",
       boxShadow: "0 2px 4px rgba(0, 0, 0, 0.1)",
       // border: "1px solid #ccc",
@@ -120,6 +139,13 @@ const StatusBar = forwardRef((props, ref) => {
         <div
           key={index}
           className={`d-flex flex-column status-card ${item.border}`}
+          onClick={() => {
+            if (pathname === item.link?.split("?")[0]) {
+              return;
+            } else {
+              navigate(item.link)
+            }
+          }}
         >
           <div className="d-flex align-items-center justify-content-between ">
             {/* Left side */}
